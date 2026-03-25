8e5f4fb7342c926a543752c41086e4092f56e6e6
Dimit3y <futuresci2035@gmail.com>
2026-03-25 20:27:50 +0500

    Add user initialization from Telegram WebApp
    Initialize user data and set player name based on Telegram user information.

--- game/client/telegram.js
+++ game/client/telegram.js
@@ -1,2 +1,8 @@
 const tg = window.Telegram.WebApp;
 tg.expand();
+
+const user = tg.initDataUnsafe?.user;
+
+window.playerName = user
+    ? (user.first_name + (user.last_name ? " " + user.last_name : ""))
+    : "Игрок";
