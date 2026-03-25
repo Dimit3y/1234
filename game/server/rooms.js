408700b44db6540a8ba0d72c7ea5614fb1ea9c85
Dimit3y <futuresci2035@gmail.com>
2026-03-25 20:29:48 +0500

    Update rooms.js

--- game/server/rooms.js
+++ game/server/rooms.js
@@ -2,23 +2,23 @@ const { v4: uuidv4 } = require('uuid');
 
 const rooms = {};
 
-function createRoom(ws) {
+function createRoom(ws, name = "Игрок") {
     const id = uuidv4();
 
     rooms[id] = {
         id,
-        players: [ws],
+        players: [{ ws, name }],
         game: null
     };
 
     return rooms[id];
 }
 
-function joinRoom(id, ws) {
+function joinRoom(id, ws, name = "Игрок") {
     const room = rooms[id];
     if (!room || room.players.length >= 2) return false;
 
-    room.players.push(ws);
+    room.players.push({ ws, name });
     return true;
 }
 
